499b9511d1daa3e19de8c7fd235977adaf2e5ac2
Dimuthu Lankanatha Hemachandra Henadeerage Don <dimuthu@gene.stanford.edu>
2025-01-02 19:30:12 -0800

    cleaning up

--- unzip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#takes path as input. uses cwd if not provided
-DIRECTORY="${1:-$(pwd)}"
-
-#use find to locate all .zip files and unzip them in place (in the same directory)
-find "$DIRECTORY" -type f -iname "*.zip" | while read -r zipfile; do
-  unzip -o "$zipfile" -d "$(dirname "$zipfile")"
-done
